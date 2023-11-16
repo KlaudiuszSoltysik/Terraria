4bb5cd6866be083623b0f69ff97bccc14d922b88
Klaudiusz <klaudiusz.s1405@gmail.com>
2023-11-16 20:50:28 +0100

    16.11 work

--- map.cpp
+++ map.cpp
@@ -8,6 +8,8 @@
 #include <sstream>
 #include <string>
 #include <vector>
+#include <map>
+#include <memory>
 
 #include "hero.cpp"
 
@@ -18,10 +20,8 @@ class Map {
         float m_block_width;
         std::string m_contents;
         std::string m_map_filename = "map.txt";
-        sf::Texture m_texture_brick1;
-        sf::Texture m_texture_brick2;
         Hero m_hero;
-        std::vector<sf::RectangleShape> m_walls;
+        std::map<char, std::vector<sf::RectangleShape>> m_map;
     public:
         Map(sf::RenderWindow &window) : m_window(window), m_block_width(window.getSize().y / 10), m_hero(window) {
             std::ifstream map_file;
@@ -33,10 +33,6 @@ class Map {
             map_file.close();
 
             m_contents = file_contents.str();
-        }
-
-        void update() {
-            m_walls.clear();
 
             int col = 0;
             int row = 0;
@@ -44,28 +40,42 @@ class Map {
             for (char c : m_contents) {
                 sf::RectangleShape rec;
                 rec.setSize(sf::Vector2f(m_block_width, m_block_width));
-
+                sf::Vector2f position(col * m_block_width, row * m_block_width);
+            
                 if (c == '\n') {
                     row++;
                     col = 0;
                     continue;
                 } else if (c == '_') {
                     rec.setFillColor(sf::Color::Black);
+                    rec.setPosition(position);
+                    m_map['_'].push_back(rec);
                 } else if (c == '#') {
                     rec.setFillColor(sf::Color::Cyan);
+                    rec.setPosition(position);
+                    m_map['#'].push_back(rec);
                 } else if (c == '*') {
                     rec.setFillColor(sf::Color::Yellow);
-                    rec.setPosition(col * m_block_width, row * m_block_width);
-                    m_walls.push_back(rec);
+                    rec.setPosition(position);
+                    m_map['*'].push_back(rec);
                 }
-
-                rec.setPosition(col * m_block_width, row * m_block_width);
                 
                 m_window.draw(rec);
 
                 col++;
             }
-            m_hero.update(m_walls);
+        }
+
+        void update() {
+            for (auto &pair : m_map) {
+                for (sf::RectangleShape &rec : pair.second) {
+                    auto position = rec.getPosition();
+                    rec.setPosition(position.x - 1, position.y);
+                    m_window.draw(rec);
+                }
+            }
+
+            m_hero.update(m_map['*']);
             m_window.draw(m_hero);
         }
 
